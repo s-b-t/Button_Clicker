4ecaf3abc08f9b7f576751c7087bca497df5a849
Steve-Tobias <sblaket@gmail.com>
2022-09-19 23:33:18 -0600

    Button_Clicker

---
+++ script.js
@@ -0,0 +1,18 @@
+function clicked(element) {
+    element.innerText =
+    "Logout";
+}
+
+function myAlert() {
+    alert("Ninja was liked");
+}
+
+function hide(element) {
+    element.remove();
+}
+
+
+
+
+
+
